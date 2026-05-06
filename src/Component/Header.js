0ed65d54c76860849b70ba4c07a5098a071a0a26
noorulain88 <noorulainain88@gmail.com>
2026-05-06 20:04:03 +0500

    fix: image path

--- src/Component/Header.js
+++ src/Component/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function Header() {
     return (
-        <div className=" h-[100vh] bg-[url('/images/images3.jpg')] bg-contain bg-left flex items-center text-black" id="home">
+        <div className=" h-[100vh] bg-[url('/Images/images3.jpg')] bg-contain bg-left flex items-center text-black" id="home">
             <h1 className="text-5xl ml-10 pl-60  text-center  md:text-7xl lg:text-8xl font-bold text-white  ">
                     
                Nazoora Collection
--- src/Data/Products.js
+++ src/Data/Products.js
@@ -5,14 +5,14 @@ export const Products = [
     id: 1,
     name: 'Classic Carry Tote',
     price: 'Rs1500',
-    image: '/Images/Images1.jpg',
+    image: '/Images/images1.jpg',
 
   },
   {
     id: 2,
     name: 'Classic Tote',
     price: 'Rs1500',
-    image: '/Images/Images5.jpg',
+    image: '/Images/images5.jpg',
   
 
   },
@@ -20,28 +20,28 @@ export const Products = [
     id: 3,
     name: 'Everyday Classic Carryall',
     price: 'Rs1500',
-    image: '/Images/Images7.jpg',
+    image: '/Images/images7.jpg',
 
   },
   {
     id: 4,
     name: 'Iconic Classic Tote',
     price: 'Rs1500',
-    image: '/Images/Images2.jpg',
+    image: '/Images/images2.jpg',
 
   },
   {
     id: 5,
     name: 'Timeless Tote',
     price: 'Rs1500',
-    image: '/Images/Images8.avif',
+    image: '/Images/images8.avif',
 
   },
   {
     id: 6,
     name: 'Traditional Tote Bag',
     price: 'Rs1500',
-    image: '/Images/Images6.jpg',
+    image: '/Images/images6.jpg',
 
   }
 
