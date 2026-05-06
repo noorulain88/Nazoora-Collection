f0608cc1146fb15844646f4c7184820b284b5600
noorulain88 <noorulainain88@gmail.com>
2026-05-06 20:00:03 +0500

    Fix the image path by adding I in uppercase

--- src/Data/Products.js
+++ src/Data/Products.js
@@ -5,14 +5,14 @@ export const Products = [
     id: 1,
     name: 'Classic Carry Tote',
     price: 'Rs1500',
-    image: '/images/images1.jpg',
+    image: '/Images/Images1.jpg',
 
   },
   {
     id: 2,
     name: 'Classic Tote',
     price: 'Rs1500',
-    image: '/images/images5.jpg',
+    image: '/Images/Images5.jpg',
   
 
   },
@@ -20,28 +20,28 @@ export const Products = [
     id: 3,
     name: 'Everyday Classic Carryall',
     price: 'Rs1500',
-    image: '/images/images7.jpg',
+    image: '/Images/Images7.jpg',
 
   },
   {
     id: 4,
     name: 'Iconic Classic Tote',
     price: 'Rs1500',
-    image: '/images/images2.jpg',
+    image: '/Images/Images2.jpg',
 
   },
   {
     id: 5,
     name: 'Timeless Tote',
     price: 'Rs1500',
-    image: '/images/images8.avif',
+    image: '/Images/Images8.avif',
 
   },
   {
     id: 6,
     name: 'Traditional Tote Bag',
     price: 'Rs1500',
-    image: '/images/images6.jpg',
+    image: '/Images/Images6.jpg',
 
   }
 
