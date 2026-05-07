631cb4ff4eecb90e85bc2718c6a2897d6603bb5c
noorulain88 <noorulainain88@gmail.com>
2026-05-07 19:49:36 +0500

    set:navbar

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -3,11 +3,11 @@ import React from 'react'
 export default function Navbar() {
     return (
         <div className='bg-blue-300'>
-            <nav className=' p-5  flex  text-black bg-stone-250 font-xs p-2 text-lg  border    w-full    rounded-full   z-50    justify-around  shadow-2xl fixed bg-neutral-100   font-medium transition-all duration-300'>
+            <nav className=' p-5  flex  text-black bg-stone-250 font-xs p-2 text-lg  border    w-full    rounded-full   z-50    justify-around  shadow-2xl fixed  bg-neutral-100   font-medium transition-all duration-300'>
 
 
 
-                <ul className='flex space-x-4 font-medium  '>
+                <ul className='flex space-x-4 font-medium'>
 
                     <a href="#home" className='hover:scale-105 font-bold text-2xl text-black '>Home</a>
                     <a href="#products" className='hover:scale-105 font-bold text-2xl  text-black '>Products</a>
